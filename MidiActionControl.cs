c841f8e011b922898aa09148b4eb39ccfb79515b
phellutone <phellutone@gmail.com>
2021-05-19 16:46:49 +0900

    Create MidiActionControl.cs

---
+++ MidiActionControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidiActionControl : MidiDeviceActionControl
+{
+    public MidiDeviceIdentifier identifier;
+    public string controlName;
+    public bool isButton;
+    public int channel;
+    public int number;
+
+    public float value;
+    public event Action<string, float> ActionControl;
+    private event Action<float> ValueControl;
+
+    void Start()
+    {
+        if (identifier == null) return;
+        ValueControl += (value) => this.value = value;
+
+        var scheme = new MidiDeviceActionControlScheme(isButton, controlName, channel, number);
+        scheme.Init(ActionControl, ValueControl, identifier);
+    }
+}
