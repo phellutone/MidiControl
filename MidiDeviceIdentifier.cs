057ea383e23615614ceaf3db91cfc519c04d028a
phellutone <phellutone@gmail.com>
2021-05-23 14:22:44 +0900

    Update MidiDeviceIdentifier.cs

--- MidiDeviceIdentifier.cs
+++ MidiDeviceIdentifier.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -9,8 +10,15 @@ public class MidiDeviceIdentifier : MonoBehaviour
 {
     public Minis.MidiDevice[] midiDevices = new Minis.MidiDevice[16];
 
+    private List<Action<Minis.MidiDevice, bool, Minis.MidiNoteControl, Minis.MidiValueControl, float>> _channeldistributor;
+    public event Action<Minis.MidiDevice, bool, Minis.MidiNoteControl, Minis.MidiValueControl, float> ChannelDistributor
+    {
+        add { (_channeldistributor = _channeldistributor ?? new List<Action<Minis.MidiDevice, bool, Minis.MidiNoteControl, Minis.MidiValueControl, float>>()).Add(value); }
+        remove { _channeldistributor.Remove(value); }
+    }
+
     [SerializeField]
-    public List<bool> _channels = new List<bool>()
+    public List<bool> channels = new List<bool>()
     {
         false,
         false,
@@ -31,71 +39,82 @@ public class MidiDeviceIdentifier : MonoBehaviour
     };
 
     [SerializeField]
-    public string _productName;
+    public string productName;
 
-    public string _product;
-    public int _channel;
-    public int _number;
-    public string _displayName;
-    public float _value;
+    public string product;
+    public int channel;
+    public int number;
+    public bool isButton;
+    public string displayName;
+    public float value;
 
-    private bool DeviceSearcher(InputDevice dev)
+    private Minis.MidiDevice DeviceSearcher(InputDevice dev)
     {
         InputDeviceMatcher match = new InputDeviceMatcher();
         match = match.WithInterface("Minis");
-        match = match.WithProduct(_productName);
+        match = match.WithProduct(this.productName);
 
         if (match.MatchPercentage(dev.description) > 0)
-        {
-            var midiDevice = dev as Minis.MidiDevice;
-            if (midiDevice == null) return false;
+            return dev as Minis.MidiDevice;
+        else
+            return null;
+    }
 
-            midiDevices[midiDevice.channel] = midiDevice;
-            return true;
-        }
-        return false;
+    private void Distributor(Minis.MidiDevice dev, bool isb, Minis.MidiNoteControl note, Minis.MidiValueControl ctrl, float val)
+    {
+        if (_channeldistributor != null)
+            foreach (var action in _channeldistributor)
+                action(dev, isb, note, ctrl, val);
+    }
+
+    private void Inspector(Minis.MidiDevice dev, bool isb, int num, string name, float val)
+    {
+        this.product = dev.description.product;
+        this.channel = dev.channel;
+        this.number = num;
+        this.isButton = isb;
+        this.displayName = name;
+        this.value = val;
     }
 
     private void Awake()
     {
+        if (this.productName == "")
+            this.productName = gameObject.name;
+
         InputSystem.onDeviceChange += (device, change) =>
         {
-            if (!DeviceSearcher(device)) return;
-            var midiDevice = device as Minis.MidiDevice;
+            var midiDevice = DeviceSearcher(device);
             if (midiDevice == null) return;
 
             if (change == InputDeviceChange.Added || change == InputDeviceChange.Reconnected || change == InputDeviceChange.Enabled)
-                _channels[midiDevice.channel] = midiDevices[midiDevice.channel] is Minis.MidiDevice;
+            {
+                this.channels[midiDevice.channel] = true;
+                midiDevices[midiDevice.channel] = midiDevice;
+            }
             else
-                _channels[midiDevice.channel] = false;
-
-            if (change != InputDeviceChange.Added) return;
+            {
+                this.channels[midiDevice.channel] = false;
+                midiDevices[midiDevice.channel] = null;
+                return;
+            }
 
             midiDevice.onWillNoteOn += (note, velocity) =>
             {
-                _product = midiDevice.description.product;
-                _channel = midiDevice.channel;
-                _number = note.noteNumber;
-                _displayName = note.displayName;
-                _value = velocity;
+                Distributor(midiDevice, true, note, null, velocity);
+                Inspector(midiDevice, true, note.noteNumber, note.displayName, velocity);
             };
 
             midiDevice.onWillNoteOff += (note) =>
             {
-                _product = midiDevice.description.product;
-                _channel = midiDevice.channel;
-                _number = note.noteNumber;
-                _displayName = note.displayName;
-                _value = 0;
+                Distributor(midiDevice, true, note, null, 0);
+                Inspector(midiDevice, true, note.noteNumber, note.displayName, 0);
             };
 
             midiDevice.onWillControlChange += (control, velocity) =>
             {
-                _product = midiDevice.description.product;
-                _channel = midiDevice.channel;
-                _number = control.controlNumber;
-                _displayName = control.displayName;
-                _value = velocity;
+                Distributor(midiDevice, false, null, control, velocity);
+                Inspector(midiDevice, false, control.controlNumber, control.displayName, velocity);
             };
         };
     }
@@ -107,13 +126,14 @@ public class MidiDeviceIdentifierEditor : Editor
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_productName"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_channels"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_product"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_number"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_channel"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_displayName"), true);
-        EditorGUILayout.PropertyField(serializedObject.FindProperty("_value"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("productName"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("channels"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("product"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("channel"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("number"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("isButton"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("displayName"), true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("value"), true);
         serializedObject.ApplyModifiedProperties();
     }
 }
